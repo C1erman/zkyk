ef420b1df0fd3c1d72f19b675664dddb0f2eef09
c1er <kexiezhang@gmail.com>
2020-10-09 22:04:22 +0800

    Modify Drawer component

--- zkyk-hurry/src/components/Drawer/index.js
+++ zkyk-hurry/src/components/Drawer/index.js
@@ -13,16 +13,16 @@ const Drawer = ({
         document.body.style.overflow = visible ? 'hidden' : '';
     }, [visible]);
     const handleClick = () => {
-        if(visible){
-            ref.current.style = 'transform: translateX(100%)';
-        }
-        else{
-            ref.current.style = 'transform: translateX(0)';
-        }
+        // if(visible){
+        //     ref.current.style = 'transform: translateX(100%)';
+        // }
+        // else{
+        //     ref.current.style = 'transform: translateX(0)';
+        // }
     }
     return (
         <div className='drawer-container'>
-            <div className={visible ? 'drawer drawer-open' : 'drawer'}>
+            <div className={ 'drawer' + ( visible ? ' drawer-open' : '' )}>
                 <div className='drawer-mask' ref={maskRef}></div>
                 <div className='drawer-content-container' ref={ref}>
                     <div className='drawer-entry' onClick={() => {
