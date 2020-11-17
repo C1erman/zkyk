3a4660ef7836a0a0c50bda5781380f05c93a4ca0
c1erman <kexiezhang@gmail.com>
2020-11-17 19:05:04 +0800

    Add zkyk-taro project

---
+++ zkyk-taro/src/pages/index/index.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react'
+import { View, Text} from '@tarojs/components'
+import './index.css'
+
+const Index = () => {
+  
+
+  return (
+    <View>hello world</View>
+  );
+}
+
+export default Index
+
